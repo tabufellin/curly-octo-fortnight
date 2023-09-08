243ae08997c1a0082c61aa02ca5ca0e50da070ba
WiCald <99890584+WiCald@users.noreply.github.com>
2023-09-07 20:56:10 -0600

    Update MainActivity.kt

--- app/src/main/java/com/example/remindme/MainActivity.kt
+++ app/src/main/java/com/example/remindme/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.remindme
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.remindme.ui.theme.RemindMeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,28 +25,48 @@ class MainActivity : ComponentActivity() {
             RemindMeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxSize().background(Color(0xFF0000FF)),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(text = "Remind Me", style = MaterialTheme.typography.headlineMedium)
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Button(onClick = {
+                            val intent = Intent(this@MainActivity, SecondActivity::class.java)
+                            startActivity(intent)
+                        }) {
+                            Text(text = "Sign in with Google")
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainScreenPreview() {
     RemindMeTheme {
-        Greeting("Android")
+        Surface(
+            modifier = Modifier.fillMaxSize().background(Color(0xFF0000FF)),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "Remind Me", style = MaterialTheme.typography.headlineMedium)
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(onClick = {}) {
+                    Text(text = "Sign in with Google")
+                }
+            }
+        }
     }
 }
